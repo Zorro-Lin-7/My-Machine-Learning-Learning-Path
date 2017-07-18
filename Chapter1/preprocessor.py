7c60c1718598edd709b9bbab6213e8a34ccc2947
linzhun <linzhun7@gmail.com>
2017-07-18 10:12:51 +0800

    OneHotEncoding

--- Chapter1/preprocessor.py
+++ Chapter1/preprocessor.py
@@ -15,6 +15,19 @@ data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 data_scaled = data_scaler.fit_transform(data)
 print('\nMin max scaled data =\n', data_scaled)
 
-#data normalization
+# data normalization
 data_normalized = preprocessing.normalize(data, norm='l1')
 print('\nL1 normalized data =\n', data_normalized)
+
+# Binarization
+data_binarized = preprocessing.Binarizer(threshold=1.4).transform(data)
+print("\nBinarized data =\n", data_binarized)
+
+# One-Hot Encoding  # 非1的值全变为0
+encoder = preprocessing.OneHotEncoder()
+encoder.fit([[0, 2, 1, 12],
+             [1, 3, 5, 3],
+             [2, 3, 2, 12],
+             [1, 2, 4, 3]])
+encoded_vector = encoder.transform([[2, 3, 5, 3]]).toarray()
+print('\nEncoded vector =', encoded_vector)
