2dee6a3d117039a1c861d424c1ec5499851ec0c5
linzhun <linzhun7@gmail.com>
2017-07-19 17:07:34 +0800

    accuracy;model persistence

--- Chapter1/regressor.py
+++ Chapter1/regressor.py
@@ -48,3 +48,26 @@ plt.scatter(X_test, y_test, color='green')
 plt.plot(X_test, y_test_pred, color='black', linewidth=4)
 plt.title('Test data')
 plt.show()
+
+# Measure performance
+import sklearn.metrics as sm
+
+print('Mean absolute error =', round(sm.mean_absolute_error(y_test,y_test_pred),2))
+print('Mean squred error =', round(sm.mean_squared_error(y_test, y_test_pred), 2))
+print('Median absolute error =', round(sm.median_absolute_error(y_test, y_test_pred),2))
+print('Explain variance score =', round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print('R2 score =', round(sm.r2_score(y_test, y_test_pred), 2))
+
+# Model persistence
+import cPickle as pickle
+
+output_model_file = '3_model_linear_regr.pkl'
+
+with open(output_model_file, 'w') as f:
+    pickle.dump(linear_regressor, f)
+    
+with open(output_model_file, 'r') as f:
+    model_linregr = pickle.load(f)
+    
+y_test_pred_new = model_linregr.predict(X_test)
+print('\nNew mean absolute error =', round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
