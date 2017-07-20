9c092a2dd11fb537ffc282f3411118b4eb47df7a
linzhun <linzhun7@gmail.com>
2017-07-20 11:47:11 +0800

    ridge regression;polynomial regression

--- Chapter1/regressor.py
+++ Chapter1/regressor.py
@@ -58,7 +58,7 @@ print('Median absolute error =', round(sm.median_absolute_error(y_test, y_test_p
 print('Explain variance score =', round(sm.explained_variance_score(y_test, y_test_pred), 2))
 print('R2 score =', round(sm.r2_score(y_test, y_test_pred), 2))
 
-# Model persistence
+Model persistence
 import cPickle as pickle
 
 output_model_file = '3_model_linear_regr.pkl'
@@ -71,3 +71,17 @@ with open(output_model_file, 'r') as f:
     
 y_test_pred_new = model_linregr.predict(X_test)
 print('\nNew mean absolute error =', round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
+
+Ridge Regressor
+
+ridge_regressor = linear_model.Ridge(alpha=0.01, fit_intercept=True, max_iter=10000)
+# The alpha parameter controls the complexity.
+
+ridge_regressor.fit(X_train, y_train)
+y_test_pred_ridge = ridge_regressor.predict(X_test)
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_ridge), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred_ridge), 2)) 
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred_ridge), 2))
+print("Explained variance score =", round(sm.explained_variance_score(y_test, y_test_pred_ridge), 2))
+print("R2 score =", round(sm.r2_score(y_test, y_test_pred_ridge), 2))
+
