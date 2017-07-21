03ccba710bb1f45695cdb2bed158557e4f8dbe0d
linzhun <linzhun7@gmail.com>
2017-07-21 19:49:39 +0800

    diving to NLTK in 2 ways

---
+++ NLP/NLTK Essentials/Chapter01/diving_to_nltk_python2.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+# 重要：python2 必须要有上述编码声明
+
+# python2 不用NLTK的方式，提取文本并分词
+
+import urllib2
+
+responses = urllib2.urlopen("http://python.org/") # 下载网页数据
+html = responses.read()  # 读取网页数据（包含hmtl）
+print len(html)
+
+# tokens = [tok for tok in html.split()]   #分词
+# print "Total number of tokens :"+ str(len(tokens))  #统计词数
+# print tokens[0:20]  
+# 除了文本内容，还含有html标签，需要再清洗，以下用re直接剥离提炼
+
+import re
+
+tokens = re.split("\W+", html)
+print "Total number of tokens : {}".format(len(tokens))
+print tokens[:20]
+
+# 上述方式可以用nltk 实现得更简洁
+
+# 词频排序-字典按value排序
+# python 方式：
+import operator
+freq_dis = {}
+for tok in tokens:
+    if tok in freq_dis:
+        freq_dis[tok] += 1
+    else:
+        freq_dis[tok] = 1
+sorted_freq_dist = sorted(freq_dis.items(), key = operator.itemgetter(1), reverse=True)
+print sorted_freq_dist[:20]
+
+# NLTK 方式：
+import nltk
+Freq_dist_nltk = nltk.FreqDist(tokens)
+print Freq_dist_nltk
+print('\n--------------')
+for k, v in Freq_dist_nltk.items():
+    print str(k) + ':' + str(v)
+    
+Freq_dist_nltk.plot(50, cumulative=False)
---
+++ NLP/NLTK Essentials/Chapter01/python3_NLTK.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+# python3
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+web_data = requests.get("http://python.org/") # get 整个网页元数据
+python_html = web_data.text                # 元数据中提取html部分的文本
+soup = BeautifulSoup(python_html,'lxml')     # 解析html文本，好比 混合物状态的汤 变成 分层的汤
+clean = soup.get_text()   #从“分层”的汤里，提取“html文本”这一层 
+# print(clean)
+tokens = [tok for tok in clean.split()]
+print("Total number of tokens: {}".format(len(tokens)))
+print(tokens[:25])
+print("\n---------------")
+# 对比：用re貌似提纯度更高,也可能分词更细导致噪音增加
+tokens_by_re = re.split("\W+", clean)
+print("Total number of tokens_by_re: {}".format(len(tokens_by_re)))
+print(tokens_by_re[:25])
+
+import nltk
+Freq_dist_nltk = nltk.FreqDist(tokens)
+print(Freq_dist_nltk)
+print("\n------------")
+for k, v in Freq_dist_nltk.items():
+    print(str(k) + ':' + str(v))
+    
