dba9eb8c8122bb001517685d131d5ec6d1f0f598
linzhun <linzhun7@gmail.com>
2017-07-26 09:02:02 +0800

    Splitting the dataset for training and testing

--- RealWorldMachineLearning/Chapter2/naive_bayes.py
+++ RealWorldMachineLearning/Chapter2/naive_bayes.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplo as plt
+import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
 
 from LogisticRegression import plot_classifier
@@ -26,3 +26,17 @@ accuracy = 100.0 * (y == y_pred).sum() / X.shape[0]
 print("Accuracy of the classifier = ", round(accuracy, 2),'%')
 
 plot_classifier(classifier_gaussiannb, X, y)
+
+##########################################
+# Train Test split
+from sklearn import cross_validation
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=5)
+classifier_gaussiannb_new = GaussianNB()
+classifier_gaussiannb_new.fit(X_train, y_train)
+y_test_pred = classifier_gaussiannb_new.predict(X_test)
+
+accuracy = 100.0 * (y_test == y_test_pred).sum() / X_test.shape[0]
+print("Accuracy of the classifier_new = ", round(accuracy, 2), '%')
+
+plot_classifier(classifier_gaussiannb_new, X_test, y_test)
